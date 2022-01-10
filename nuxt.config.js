c864fd42ed949e6db9845864269dc6bbd49ae857
Sergii <sergiimostepan@gmail.com>
2022-01-10 17:44:58 +0200

    b

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,9 +28,9 @@ export default {
 
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  // plugins: [
-  //   '@/plugins/axios',
-  // ],
+  plugins: [
+    '@/plugins/axios',
+  ],
   router: routerConfig,
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
